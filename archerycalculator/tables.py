d24249a0980227d8a3599aa25b418bff5752186b
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-01-09 22:33:59 +0000

    Classification table printing (no filtering).

--- archerycalculator/tables.py
+++ archerycalculator/tables.py
@@ -5,7 +5,7 @@ from flask import (
 )
 import numpy as np
 
-from archerycalculator.db import get_db
+from archerycalculator.db import get_db, query_db
 
 from archeryutils import rounds
 from archeryutils.handicaps import handicap_equations as hc_eq
@@ -24,7 +24,7 @@ def handicap_tables():
 
     form = TableForm.HandicapTableForm(request.form)
 
-    all_rounds = database.execute("SELECT round_name FROM rounds").fetchall()
+    all_rounds = query_db("SELECT round_name FROM rounds")
 
     if request.method == "POST" and form.validate():
         error = None
@@ -53,19 +53,19 @@ def handicap_tables():
         rounds_req = [i for i in rounds_req if i]
         round_objs = []
         for round_i in rounds_req:
-            roundcheck = database.execute(
+            roundcheck = query_db(
                 "SELECT id FROM rounds WHERE round_name IS (?)", [round_i]
-            ).fetchall()
+            )
             if len(roundcheck) == 0:
                 error = f"Invalid round name '{round_i}'. Please select from dropdown."
 
             # Get the appropriate rounds from the database
             round_objs.append(
                 all_rounds_objs[
-                    database.execute(
+                    query_db(
                         "SELECT code_name FROM rounds WHERE round_name IS (?)",
-                        [round_i],
-                    ).fetchone()["code_name"]
+                        [round_i], one=True
+                    )["code_name"]
                 ]
             )
 
@@ -128,10 +128,10 @@ def classification_tables():
     all_bowstyles = database.execute(
         "SELECT bowstyle,disciplines FROM bowstyles"
     ).fetchall()
-    all_genders = database.execute("SELECT gender FROM genders").fetchall()
-    all_ages = database.execute("SELECT age_group FROM ages").fetchall()
-    all_rounds = database.execute("SELECT round_name FROM rounds").fetchall()
-    all_classes = database.execute("SELECT shortname FROM classes").fetchall()
+    all_genders = query_db("SELECT gender FROM genders")
+    all_ages = query_db("SELECT age_group FROM ages")
+    all_rounds = query_db("SELECT round_name FROM rounds")
+    all_classes = query_db("SELECT shortname FROM classes")
 
     form = TableForm.ClassificationTableForm(request.form)
 
@@ -146,28 +146,28 @@ def classification_tables():
         results = {}
 
         # Check the inputs are all valid
-        bowstylecheck = database.execute(
+        bowstylecheck = query_db(
             "SELECT id FROM bowstyles WHERE bowstyle IS (?)", [bowstyle]
-        ).fetchall()
+        )
         if len(bowstylecheck) == 0:
             error = "Invalid bowstyle. Please select from dropdown."
         results["bowstyle"] = bowstyle
 
-        gendercheck = database.execute(
+        gendercheck = query_db(
             "SELECT id FROM genders WHERE gender IS (?)", [gender]
-        ).fetchall()
+        )
         if len(gendercheck) == 0:
             error = "Please select gender from dropdown options."
         results["gender"] = gender
 
-        agecheck = database.execute(
+        agecheck = query_db(
             "SELECT id FROM ages WHERE age_group IS (?)", [age]
-        ).fetchall()
+        )
         if len(agecheck) == 0:
             error = "Invalid age group. Please select from dropdown."
         results["age"] = age
 
-        all_rounds_objs = rounds.read_json_to_round_dict(
+        all_rounds = rounds.read_json_to_round_dict(
             [
                 "AGB_outdoor_imperial.json",
                 "AGB_outdoor_metric.json",
@@ -178,25 +178,28 @@ def classification_tables():
             ]
         )
 
-        # Loop over all rounds to construct an object with scores for each category
-        print(all_classes[0]["shortname"])
-        # grid of rounds * classes
-        results = np.zeros([len(all_rounds_objs), 9])
-        # Loop through and get scores for HCs - don't need loop as np functionality?
-        for i, round_obj_i in enumerate(all_rounds_objs):
-            results[i, :] = np.asarray(class_func.AGB_outdoor_classification_scores(round_obj_i, bowstyle, gender, age))
-        # Print out
-        # Worry about deleting rows next...
+        results = np.zeros([len(all_rounds), len(all_classes)-1])
+        # Loop through and get scores
+        for i, round_i in enumerate(all_rounds):
+            results[i, :] = np.asarray(class_func.AGB_outdoor_classification_scores(round_i, bowstyle, gender, age))
+            # Worry about deleting rows next...
+        
+        # Add roundnames on to the end then flip for printing
+        roundnames = [round_i["round_name"] for round_i in query_db("SELECT round_name FROM rounds")]
+        results = np.flip(np.concatenate((results.astype(int), np.asarray(roundnames)[:, None]), axis=1), axis=1)
+        classes = all_classes[-2::-1]
 
         if error is None:
             # Return the results
+            # Flip array so lowest class on left for printing
             return render_template(
                 "classification_tables.html",
                 bowstyles=all_bowstyles,
                 genders=all_genders,
                 ages=all_ages,
                 form=form,
-                results=results,
+                results=results.astype(str),
+                classes=classes
             )
         else:
             # If errors reload default with error message
