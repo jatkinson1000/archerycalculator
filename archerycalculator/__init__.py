6cbebfc67a969196b0b41b2e2d76fec39d446a3f
Jack Atkinson <jwa34@cam.ac.uk>
2025-08-12 14:46:40 +0100

    Remove secret key. Not a security issue as currently unused as site does not utilise 'sessions' to track users, so was redundant.

--- archerycalculator/__init__.py
+++ archerycalculator/__init__.py
@@ -34,8 +34,7 @@ def create_app(test_config=None):
     """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "archerycalculator.sqlite"),
+        DATABASE=os.path.join(app.instance_path, "archerycalculator.sqlite")
     )
 
     if test_config is None:
